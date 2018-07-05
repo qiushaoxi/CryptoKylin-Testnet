f704f02fc97c2b280c84874e6d96298f2616b171
qiushaoxi <qiushaoxi@163.com>
2018-07-05 11:32:04 +0800

    init

---
+++ docker/set-env.sh
@@ -0,0 +1,6 @@
+export eos_data_dir=`pwd`/../data
+export eos_config_dir=`pwd`/../config
+export docker_tag=qiushaoxi/eos-mainnet:v1.0.7
+export container_name=localeos
+export http_port=8888
+export p2p_port=9876
